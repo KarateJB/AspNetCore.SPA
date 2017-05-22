bda19a6ddfb833dd0c5bc9c95264e6c1fc8a93d2
KarateJB <ppippenkimo@gmail.con>
2017-05-22 14:07:13 +0800

    Create issue: Install dotnet watch fail

--- src/AspNetCore.Spa.Website/ClientApp/app/app.module.ts
+++ src/AspNetCore.Spa.Website/ClientApp/app/app.module.ts
@@ -6,7 +6,6 @@ import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
-import { TestComponent } from './test/test.component';
 
 @NgModule({
     bootstrap: [ AppComponent ],
@@ -15,8 +14,7 @@ import { TestComponent } from './test/test.component';
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
-        HomeComponent,
-        TestComponent
+        HomeComponent
     ],
     imports: [
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
