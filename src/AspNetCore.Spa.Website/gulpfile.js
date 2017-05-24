9a0448ec3851158d988f1b1977da81a801a87d61
KarateJB <ppippenkimo@gmail.con>
2017-05-24 15:19:37 +0800

    Install some npm packages.

--- src/AspNetCore.Spa.Website/gulpfile.js
+++ src/AspNetCore.Spa.Website/gulpfile.js
@@ -1,5 +1,54 @@
 var gulp = require('gulp');
 var server = require('karma').Server;
+var root_path = {
+    webroot: "./wwwroot/"
+}
+
+//library source
+root_path.nmSrc = "./node_modules/";
+//library destination
+root_path.packageLib = root_path.webroot + "js/lib-npm/"
+
+
+//font-awesome
+gulp.task('copy-fa-css', function () {
+    return gulp.src(root_path.nmSrc + "/font-awesome/fonts/*", {
+        base: root_path.nmSrc + '/font-awesome/fonts/'
+    }).pipe(gulp.dest(root_path.packageLib + '/font-awesome/fonts/'));
+});
+
+gulp.task('copy-fa-fonts', function () {
+    return gulp.src(root_path.nmSrc + "/font-awesome/css/font-awesome.min.css", {
+        base: root_path.nmSrc + '/font-awesome/css/'
+    }).pipe(gulp.dest(root_path.packageLib + '/font-awesome/css/'));
+});
+//sweetalert2
+gulp.task('copy-sweetalert2', function () {
+    return gulp.src(root_path.nmSrc + "/sweetalert2/dist/sweetalert2*", {
+        base: root_path.nmSrc + '/sweetalert2/dist/'
+    }).pipe(gulp.dest(root_path.packageLib + '/sweetalert2/'));
+});
+//ng2-toastr
+gulp.task('copy-ng2-toastr', function () {
+    return gulp.src([
+        root_path.nmSrc + "/ng2-toastr/bundles/*.min.js",
+        root_path.nmSrc + "/ng2-toastr/bundles/*.min.css"
+    ], {
+            base: root_path.nmSrc + '/ng2-toastr/bundles/'
+        }).pipe(gulp.dest(root_path.packageLib + '/ng2-toastr/'));
+});
+
+//jsnlog
+gulp.task('copy-jsnlog', function () {
+    return gulp.src(root_path.nmSrc + "/jsnlog/jsnlog*.js", {
+        base: root_path.nmSrc + '/jsnlog/'
+    }).pipe(gulp.dest(root_path.packageLib + '/jsnlog/'));
+});
+
+
+/*
+ *Tasks
+ */
 
 gulp.task('test', function (done) {
     new server({
@@ -7,3 +56,11 @@ gulp.task('test', function (done) {
         singleRun: true
     }).start();
 });
+
+gulp.task("copy-all", [
+    "copy-fa-css",
+    "copy-fa-fonts",
+    "copy-sweetalert2",
+    "copy-ng2-toastr",
+    "copy-jsnlog"
+]);
