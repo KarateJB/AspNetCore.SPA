7e4d6801fef265b01abce8855a8171c581907981
KarateJB <ppippenkimo@gmail.con>
2017-05-28 07:59:27 +0800

    jsnlog works.

--- src/AspNetSpa.Website/ClientApp/app/components/home/home.component.ts
+++ src/AspNetSpa.Website/ClientApp/app/components/home/home.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { JL } from 'jsnlog';
 
 @Component({
     selector: 'home',
     templateUrl: './home.component.html'
 })
 export class HomeComponent {
+    constructor(){
+        JL("Angular2").debug("jsnlog works!");
+    }
 }
