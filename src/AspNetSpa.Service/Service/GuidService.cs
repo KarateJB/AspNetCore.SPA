5066dedec3a74e43b3b484dd4d4170d7c7b6ea8a
KarateJB <ppippenkimo@gmail.con>
2017-05-31 13:30:52 +0800

    Create a service library.

---
+++ src/AspNetSpa.Service/Service/GuidService.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AspNetSpa.Service
+{
+    /// <summary>
+    /// GUID Service
+    /// </summary>
+    public class GuidService : IGuidService
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ src/AspNetSpa.Service/Service/IGuidService.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace AspNetSpa.Service
+{
+    public interface IGuidService
+    {
+        Guid Id {get;set;}
+    }
+}
